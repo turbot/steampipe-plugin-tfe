0a915a5862922825e09f453eca97d497d5f62d1d
misraved <ved@turbot.com>
2022-04-20 15:12:06 +0530

    Update SDK

--- tfe/table_tfe_current_user.go
+++ tfe/table_tfe_current_user.go
@@ -3,8 +3,8 @@ package tfe
 import (
 	"context"
 
-	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
 )
 
 func tableTfeCurrentUser(ctx context.Context) *plugin.Table {
